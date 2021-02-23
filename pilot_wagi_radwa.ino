28b6bd9105f118bd52d8b34d3792a4a52e67e02f
MarcinanBarbarzynca <49427297+MarcinanBarbarzynca@users.noreply.github.com>
2021-02-23 09:52:54 +0100

    dodanie kodu

---
+++ pilot_wagi_radwa.ino
@@ -0,0 +1,48 @@
+#define EI_ARDUINO_INTERRUPTED_PIN // to enable pin states functionality 
+#include <EnableInterrupt.h>
+
+int wyslij_button = 2;
+int taruj_button = 3;
+int cofnij_button = 4;
+
+//LongPressedButttons
+bool i_p_active = false;
+long buttonTimer = 0;
+int longPressTimer = 4000;
+
+void wyslij() {
+  Serial.println("w");
+}
+
+void taruj() {
+  Serial.println("t");
+}
+
+void cofnij() {
+  Serial.println("c");
+}
+
+void setup() {
+  pinMode(wyslij_button, INPUT_PULLUP);
+  pinMode(taruj_button, INPUT_PULLUP);
+  pinMode(cofnij_button, INPUT_PULLUP);
+
+  enableInterrupt(wyslij_button, wyslij, FALLING);
+  enableInterrupt(taruj_button, taruj, FALLING);
+  //enableInterrupt(cofnij_button, interruptFunction,CHANGE);
+
+}
+
+void loop() {
+  if (digitalRead(cofnij_button) == LOW) {
+    if (i_p_active == false) {
+      i_p_active = true;
+      buttonTimer = millis();
+    }//Zapamietuje stan nacisniecia przycisku
+    if ((millis() - buttonTimer) > longPressTimer) {
+      cofnij();
+    } else {}
+  } else {
+    i_p_active = false;
+  }
+}
