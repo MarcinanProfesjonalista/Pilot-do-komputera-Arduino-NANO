f31f6cfe54ef42cd390d625111667e735d0accdc
MarcinanBarbarzynca <49427297+MarcinanBarbarzynca@users.noreply.github.com>
2021-06-10 15:33:49 +0200

    Utrudnienie dla pianistów
    Teraz przycisk blokuje się jeżeli zostanie naciśnięty zbyt często.

--- pilot_wagi_radwa.ino
+++ pilot_wagi_radwa.ino
@@ -12,42 +12,64 @@ int longPressTimer = 4000;
 bool timerPassed = false; 
 
 void wyslij() {
-  Serial.println("w");
-}
+      //dodałem debouncing bo zliczało nieraz 3 krotnie więcej niż powinno.
+    static unsigned long last_interrupt_time = 0;
+    unsigned long interrupt_time = millis();
+    // If interrupts come faster than 1000ms, assume it's a bounce and ignore
+    if (interrupt_time - last_interrupt_time > 1000)
+    {
+      Serial.println("w");
 
-void taruj() {
-  Serial.println("t");
-}
+    }
+    last_interrupt_time = interrupt_time;
+  }
+  
 
-void cofnij() {
-  Serial.println("c");
-}
+
+void taruj() {      //dodałem debouncing bo zliczało nieraz 3 krotnie więcej niż powinno.
+    static unsigned long last_interrupt_time = 0;
+    unsigned long interrupt_time = millis();
+    // If interrupts come faster than 1000ms, assume it's a bounce and ignore
+    if (interrupt_time - last_interrupt_time > 1000)
+    {
+      Serial.println("t");
+
+    }
+    last_interrupt_time = interrupt_time;
+  }
+  
+
+
+void cofnij() {    static unsigned long last_interrupt_time = 0;
+    unsigned long interrupt_time = millis();
+    // If interrupts come faster than 1000ms, assume it's a bounce and ignore
+    if (interrupt_time - last_interrupt_time > 1000)
+    {
+      Serial.println("c");
+
+    }
+    last_interrupt_time = interrupt_time;
+  }
+  
+
 
 void setup() {
+  Serial.begin(9600);
   pinMode(wyslij_button, INPUT_PULLUP);
   pinMode(taruj_button, INPUT_PULLUP);
   pinMode(cofnij_button, INPUT_PULLUP);
 
-  enableInterrupt(wyslij_button, wyslij, FALLING);
-  enableInterrupt(taruj_button, taruj, FALLING);
+  enableInterrupt(wyslij_button, wyslij, RISING);
+  enableInterrupt(taruj_button, taruj, RISING);
   //enableInterrupt(cofnij_button, interruptFunction,CHANGE);
 
 }
 
 void loop() {
+  //Serial.println("d");
   if (digitalRead(cofnij_button) == HIGH) {  //Tu jest odwróciona logika ze względu na to że został zastosowany CZERWONY przycisk NC. 
-    if (i_p_active == false) {
-      i_p_active = true;
-      buttonTimer = millis();
-    }//Zapamietuje stan nacisniecia przycisku
-    if ((millis() - buttonTimer) > longPressTimer) {
-      if (timerPassed == false){
+    
         cofnij(); 
-        timerPassed = true;
-      }
-    } else {}
-  } else {
-    i_p_active = false;
-    timerPassed = false;
+
   }
 }
