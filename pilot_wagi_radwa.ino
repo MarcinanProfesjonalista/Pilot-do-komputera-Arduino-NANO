b6484f0dbadf2b0b8b4860c360438c654f4f8031
MarcinanBarbarzynca <49427297+MarcinanBarbarzynca@users.noreply.github.com>
2021-09-09 13:06:37 +0200

    update for controller

--- pilot_wagi_radwa.ino
+++ pilot_wagi_radwa.ino
@@ -1,6 +1,8 @@
 #define EI_ARDUINO_INTERRUPTED_PIN // to enable pin states functionality 
 #include <EnableInterrupt.h>
 
+String identyfikator="pilot_1";
+
 int wyslij_button = 2;
 int taruj_button = 3;
 int cofnij_button = 4;
@@ -18,7 +20,7 @@ void wyslij() {
     // If interrupts come faster than 1000ms, assume it's a bounce and ignore
     if (interrupt_time - last_interrupt_time > 1000)
     {
-      Serial.println("w");
+      Serial.print("w\n\r");
 
     }
     last_interrupt_time = interrupt_time;
@@ -32,7 +34,7 @@ void taruj() {      //dodałem debouncing bo zliczało nieraz 3 krotnie więcej 
     // If interrupts come faster than 1000ms, assume it's a bounce and ignore
     if (interrupt_time - last_interrupt_time > 1000)
     {
-      Serial.println("t");
+      Serial.print("t\n\r");
 
     }
     last_interrupt_time = interrupt_time;
@@ -45,7 +47,7 @@ void cofnij() {    static unsigned long last_interrupt_time = 0;
     // If interrupts come faster than 1000ms, assume it's a bounce and ignore
     if (interrupt_time - last_interrupt_time > 1000)
     {
-      Serial.println("c");
+      Serial.print("c\n\r");
 
     }
     last_interrupt_time = interrupt_time;
@@ -66,7 +68,35 @@ void setup() {
 }
 
 void loop() {
-  //Serial.println("d");
+
+if (Serial.available()) {
+String wiadomosc = Serial.readStringUntil('\n');
+    
+    //Serial.println("WIADOMOSC: "+wiadomosc);
+    if (wiadomosc == "C1\r") {
+      Serial.print("C1 A\r\n");
+
+    }
+    if (wiadomosc == "C0\r") {
+      Serial.print("C0 A\r\n");
+    
+    }
+    if (wiadomosc == "NB\r") {
+      Serial.print("NB A"); 
+      Serial.print(" \"");
+      Serial.print(identyfikator);
+Serial.print("\"");
+      Serial.print("\r\n");
+     // Serial.print("NB A 999999\r\n");
+    }
+    if (wiadomosc == "?") {
+      Serial.println("Komendy: C0@CRLF; C1@CRLF;  NB@CRLF");
+    }
+    if (wiadomosc == "?\r\n") {
+      Serial.println("Komendy: C0@CRLF; C1@CRLF;  NB@CRLF");
+    }
+  }
+  
   if (digitalRead(cofnij_button) == HIGH) {  //Tu jest odwróciona logika ze względu na to że został zastosowany CZERWONY przycisk NC. 
     
         cofnij(); 
